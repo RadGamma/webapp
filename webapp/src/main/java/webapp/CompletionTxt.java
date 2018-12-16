8aa81c42d5e8476493cf38a95c6a068b8b39366e
Frazee <Frazee@DESKTOP-03CR761>
2018-12-16 08:13:03 -0500

    Typo change related to the security patch

--- webapp/src/main/java/webapp/CompletionTxt.java
+++ webapp/src/main/java/webapp/CompletionTxt.java
@@ -68,7 +68,7 @@ public class CompletionTxt {
  
 		// Enter your correct gmail UserID and Password
 		// if you have 2FA enabled then provide App Specific Password
-		transport.connect(emailConnectStmp, "emailConnectURL", "emailConnectionPWD");
+		transport.connect(emailConnectStmp, emailConnectURL, emailConnectionPWD);
 		transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
 		transport.close();
 	}
